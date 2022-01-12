80c5996d2b7881a78e9d35a9973ac787ec2b1fba
Jensen <2071630868@qq.com>
2022-01-12 12:28:29 +0800

    新增dog类

--- My_python/main_student.py
+++ My_python/main_student.py
@@ -6,6 +6,12 @@ Created on Wed Jan 12 11:00:08 2022
 """
 
 from Student import Student
+from Student import Dog
 student1=Student('Martin')
 student1.add_subject('biomechanics_2020')
 student1.get_student_data()
+
+        
+my_dog=Dog('Willie',6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
